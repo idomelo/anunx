e97c30885f1ac469afe7dd49299c835dcc231e4f
Ido Melo <idomelo123@gmail.com>
2022-02-28 14:24:07 -0300

    header - adicionando container e menu do usuario

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,9 @@ export default function Home() {
           Destaques
         </Typography>
         <br />
-        <Grid container spacing={4}>
+        <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={4}>
-            <Card>
+            <Card sx={{ maxWidth: 300 }}>
               <CardMedia
                 image={'https://source.unsplash.com/random'}
                 sx={{ paddingTop: '56%'}}
@@ -64,7 +64,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
-            <Card>
+            <Card sx={{ maxWidth: 300 }}>
               <CardMedia
                 image={'https://source.unsplash.com/random'}
                 sx={{ paddingTop: '56%'}}
@@ -82,7 +82,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
-            <Card>
+            <Card sx={{ maxWidth: 300 }}>
               <CardMedia
                 image={'https://source.unsplash.com/random'}
                 sx={{ paddingTop: '56%'}}
@@ -100,7 +100,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
-            <Card>
+            <Card sx={{ maxWidth: 300 }}>
               <CardMedia
                 image={'https://source.unsplash.com/random'}
                 sx={{ paddingTop: '56%'}}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,66 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from 'react'
+import Link from 'next/link'
+
+import {
+  Container,
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
+} from '@mui/material'
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 export default function ButtonAppBar() {
+  const [ anchorUseMenu, setAnchorUserMenu ] = useState(false)
+
+  const openUserMenu = Boolean(anchorUseMenu)
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Anunx
+            </Typography>
+            <Link href="/user/publish">
+              <Button color="inherit" variant="outlined">Anunciar e Vender</Button>
+            </Link>
+
+            <Button sx={{ marginLeft: 2 }} onClick={(e) => setAnchorUserMenu(e.currentTarget)}
+              size="medium" 
+              color="secondary" startIcon={
+                true === false
+                  ? <Avatar src="" />
+                  : <AccountCircleIcon />
+            } >
+              Ido F. Melo
+            </Button>
+
+            <Menu
+              anchorEl={anchorUseMenu}
+              open={openUserMenu}
+              onClose={() => setAnchorUserMenu(null)}
+            >
+              <Link href="/user/dashboard">
+                <MenuItem>Meus Anúncios</MenuItem>
+              </Link>
+              <Link href="/user/publish">
+                <MenuItem>Publicar Novo Anúncio</MenuItem>
+              </Link>
+              <Divider />
+              <MenuItem>Sair</MenuItem>
+            </Menu>
+
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
-  );
+  )
 }
