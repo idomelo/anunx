984f0ccbc85d052f64f7dd9e25d7ba57afd6da67
Ido Melo <idomelo123@gmail.com>
2022-03-22 17:04:48 -0300

    refatorando header

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,51 +18,64 @@ import {
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 export default function ButtonAppBar() {
-  const [ anchorUseMenu, setAnchorUserMenu ] = useState(false)
+  const [ anchorUserMenu, setAnchorUserMenu ] = useState(false)
   const { data: session  } = useSession()
 
-  const openUserMenu = Boolean(anchorUseMenu)
+  const openUserMenu = Boolean(anchorUserMenu)
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, justifyContent: 'space-evenly'}}>
       <AppBar position="static">
         <Container maxWidth="lg">
-          <Toolbar>
-            <Link passHref href='/'>
-              <Typography variant="h6" component="div" sx={{ cursor: 'pointer' }}>
-                Anunx
-              </Typography>
-            </Link>
-            <Box sx={{ flexGrow: 1 }}>
+          <Toolbar disableGutters>
 
+            {/* Logotipo com link */}
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ cursor: 'pointer', flexGrow: 1, display: 'flex' }}
+            >
+              <Link passHref href='/'>
+                Anunx
+              </Link>
+            </Typography>
+
+            {/* Botão anunciar e vender OU Avatar do usuário */}
+
+            <Box sx={{ flexGrow: 0, display:'flex' }}>
+              {
+                !session 
+                  ? (
+                    <Link passHref href='/auth/signin'>
+                      <Button 
+                        color="inherit" 
+                        variant="outlined" 
+                        sx={{ display: 'flex' }}
+                      >
+                        Anunciar e Vender
+                      </Button>
+                    </Link>
+                  ) : <Button 
+                        sx={{ marginLeft: 2 }} 
+                        onClick={(e) => setAnchorUserMenu(e.currentTarget)}
+                        size="medium" 
+                        color="secondary" startIcon=
+                        {
+                          session.user.image
+                            ? <Avatar src={session.user.image} />
+                            : <AccountCircleIcon />
+                        }>
+                        {session.user.name}
+                      </Button>
+              }
             </Box>
 
-            <Link passHref 
-            href={session ? '/user/publish' : '/auth/signin'}
-            
-            >
-              <Button color="inherit" variant="outlined" sx={{ display: { xs: 'none', md: 'flex' } }}>Anunciar e Vender</Button>
-            </Link>
-
-            {
-              session 
-                ? (
-                  <Button sx={{ marginLeft: 2 }} onClick={(e) => setAnchorUserMenu(e.currentTarget)}
-                    size="medium" 
-                    color="secondary" startIcon={
-                      session.user.image
-                        ? <Avatar src={session.user.image} />
-                        : <AccountCircleIcon />
-                  }>
-                    {session.user.name}
-                  </Button>
-
-                ) : null
-            }
-
+            {/* Menu do Usuário */}
 
             <Menu
-              anchorEl={anchorUseMenu}
+              anchorEl={anchorUserMenu}
               open={openUserMenu}
               onClose={() => setAnchorUserMenu(null)}
             >
